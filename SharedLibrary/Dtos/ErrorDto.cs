8f07c48464c61fbe530512474b8817ea7b3da190
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-04 22:51:30 +0300

    Shared Layer 'a ErrorDto eklendi.

--- SharedLibrary/Dtos/ErrorDto.cs
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -24,7 +24,7 @@ namespace SharedLibrary.Dtos
 
         public ErrorDto(List<string> errors, bool isShow)
         {
-            Errors = Errors;
+            Errors = errors;
             IsShow = isShow;
         }
     }
