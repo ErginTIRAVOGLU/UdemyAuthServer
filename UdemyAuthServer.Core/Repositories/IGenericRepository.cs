49155fdcc1a97861cb0045cb30a0f3cba4de9ae5
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-05 00:19:07 +0300

    IService interface eklendi.

--- UdemyAuthServer.Core/Repositories/IGenericRepository.cs
+++ UdemyAuthServer.Core/Repositories/IGenericRepository.cs
@@ -10,7 +10,7 @@ namespace UdemyAuthServer.Core.Repositories
     public interface IGenericRepository<TEntity> where TEntity : class
     {
         Task<TEntity> GetByIdAsync(int id);
-        Task<IQueryable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAllAsync();
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
         void Remove(TEntity entity);
