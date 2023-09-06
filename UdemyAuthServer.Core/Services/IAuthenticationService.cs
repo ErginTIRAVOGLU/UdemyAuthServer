3459695bf81dc8749bd1e119c0c76986481aac38
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-06 16:07:48 +0300

    Authentication Service sınıf 'ı eklendi.

--- UdemyAuthServer.Core/Services/IAuthenticationService.cs
+++ UdemyAuthServer.Core/Services/IAuthenticationService.cs
@@ -13,7 +13,7 @@ namespace UdemyAuthServer.Core.Services
         Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
         Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
         Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
-        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+        Response<ClientTokenDto> CreateTokenByClient(ClientLoginDto clientLoginDto);
 
     }
 }
